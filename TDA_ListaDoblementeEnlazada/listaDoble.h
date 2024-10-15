5fd16c40c224302533e334a1c8b32a1c0f3b5c8f
Matias Guliano <matiastrex1@gmail.com>
2024-10-15 15:17:09 -0300

    Pruebas de eliminarDeLista

--- TDA_ListaDoblementeEnlazada/listaDoble.h
+++ TDA_ListaDoblementeEnlazada/listaDoble.h
@@ -20,8 +20,10 @@ void crearListaDoble(tLista* lista);
 int listaDobleVacia(const tLista* lista);
 int listaDobleLlena(const tLista* lista, unsigned tamDato);
 int insertarOrdenadoEnListaDoble(tLista* lista, void* dato, unsigned bytesRegistro,int duplicados, int cmp(const void* a, const void* b));
+
 int eliminarDeListaDobleOrdenada(tLista* lista, void* clave, unsigned bytesRegistro,int cmp(const void* a, const void* b));
 int eliminarDeListaDobleDesordenada(tLista* lista, void* clave, unsigned bytesRegistro,int cmp(const void* a, const void* b));
+
 void recorrerListaDobleDesdeInicio(tLista* lista, void accion(void* a, void* b));
 void recorrerListaDobleDesdeFinal(tLista* lista, void accion(void* a, void* b));
 int verActual(const tLista* lista, void* buffer, unsigned bytesRegistro);
--- TDA_ListaDoblementeEnlazada/main.c
+++ TDA_ListaDoblementeEnlazada/main.c
@@ -1,32 +1,77 @@
 #include "listaDoble.h"
+//INTEGRANTES
+//Quispe Milagros 45064110
+//Cruz Aro Melina 43444249
+//Familia Cruz Karina Soledad 42838266
+//Contti Claudio Abel 39487489
+//Modestti Tomas Agustin 45073572
+//Matías Guliano 41464442
+
 int cmpEntero (const void* a, const void* b);
 void mostrarEntero(void* a, void* b);
+
 int main()
 {
-    tLista lista;
+    tLista listaOrdenada, listaDesordenada;
     int vec[] = {5,7,3,2,10,1,5}, i, num=10;
-    crearListaDoble(&lista);
+    crearListaDoble(&listaOrdenada);
+    crearListaDoble(&listaDesordenada);
+
+    //PROBANDO ELIMINAR CON LISTA VACIA
+    eliminarDeListaDobleDesordenada(&listaDesordenada,&num,sizeof(int),cmpEntero);
+    eliminarDeListaDobleOrdenada(&listaOrdenada,&num,sizeof(int),cmpEntero);
+
+    //CARGANDO LA LISTA Y MOSTRANDOLA DESDE AMBOS LADOS
     for(i=0;i<sizeof(vec)/sizeof(int);i++)
     {
-        insertarOrdenadoEnListaDoble(&lista,&vec[i],sizeof(int),NODUP,cmpEntero);
+        insertarOrdenadoEnListaDoble(&listaDesordenada,&vec[i],sizeof(int),NODUP,cmpEntero);
+        insertarOrdenadoEnListaDoble(&listaOrdenada,&vec[i],sizeof(int),NODUP,cmpEntero);
     }
     puts("Lista cargada");
-    recorrerListaDobleDesdeInicio(&lista,mostrarEntero);
+    recorrerListaDobleDesdeInicio(&listaDesordenada,mostrarEntero);
     puts("");
-    recorrerListaDobleDesdeFinal(&lista,mostrarEntero);
+    recorrerListaDobleDesdeFinal(&listaOrdenada,mostrarEntero);
 
-    eliminarDeListaDobleOrdenada(&lista,&num,sizeof(int),cmpEntero);
-    puts("\nLista tras eliminar un elemento ");
-    recorrerListaDobleDesdeInicio(&lista,mostrarEntero);
+    //PROBANDO ELIMINAR ULTIMO NODO
+    eliminarDeListaDobleOrdenada(&listaDesordenada,&num,sizeof(int),cmpEntero);
+    eliminarDeListaDobleOrdenada(&listaOrdenada,&num,sizeof(int),cmpEntero);
+
+    puts("\nLista tras eliminar ultimo elemento ");
+    recorrerListaDobleDesdeInicio(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeInicio(&listaOrdenada,mostrarEntero);
     puts("");
-    recorrerListaDobleDesdeFinal(&lista,mostrarEntero);
+    recorrerListaDobleDesdeFinal(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeFinal(&listaOrdenada,mostrarEntero);
 
+    //PROBANDO ELIMINAR PRIMER NODO
     num = 1;
-    eliminarDeListaDobleDesordenada(&lista,&num,sizeof(int),cmpEntero);
-    puts("\nLista tras eliminar un elemento ");
-    recorrerListaDobleDesdeInicio(&lista,mostrarEntero);
+    eliminarDeListaDobleDesordenada(&listaDesordenada,&num,sizeof(int),cmpEntero);
+    eliminarDeListaDobleDesordenada(&listaOrdenada,&num,sizeof(int),cmpEntero);
+
+    puts("\nLista tras eliminar primer elemento ");
+    recorrerListaDobleDesdeInicio(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeInicio(&listaOrdenada,mostrarEntero);
     puts("");
-    recorrerListaDobleDesdeFinal(&lista,mostrarEntero);
+    recorrerListaDobleDesdeFinal(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeFinal(&listaOrdenada,mostrarEntero);
+
+    //PROBANDO ELIMINAR NODO EN EL MEDIO
+    num = 5;
+    eliminarDeListaDobleDesordenada(&listaDesordenada,&num,sizeof(int),cmpEntero);
+    eliminarDeListaDobleDesordenada(&listaOrdenada,&num,sizeof(int),cmpEntero);
+
+    puts("\nLista tras eliminar elemento intermedio");
+    recorrerListaDobleDesdeInicio(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeInicio(&listaOrdenada,mostrarEntero);
+    puts("");
+    recorrerListaDobleDesdeFinal(&listaDesordenada,mostrarEntero);
+    printf(" || ");
+    recorrerListaDobleDesdeFinal(&listaOrdenada,mostrarEntero);
 
     return 0;
 }
