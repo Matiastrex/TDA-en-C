9d3fde3e8b7dd61450d6a963d2854df2162b19a4
Matias Guliano <matiastrex1@gmail.com>
2024-10-31 21:40:13 -0300

    correcion macro minimo

--- TDA_Arbol/arbolBinario.h
+++ TDA_Arbol/arbolBinario.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MINIMO (x,y) ((x)<(y)?(x):(y))
+#define MINIMO(x,y) ((x)<(y)?(x):(y))
 /** ***************************************************************************************************************************************
  **                                     ARBOLES
  ** + Por cantidad de nodos, pueden clasificarse en dos tipos:
